45d5fca1ac6c04087637a1bdcb780a91980cef5f
hivongseth2 <99702632+hivongseth2@users.noreply.github.com>
2023-10-05 00:39:13 +0700

    screen 2

---
+++ NativeTH2/Screen/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text, Button } from "react-native";
+
+export default function Home({ navigation }) {
+  return (
+    <View>
+      <Button
+        title="Go to Screen First"
+        onPress={() => navigation.navigate("Screen2")}
+      />
+    </View>
+  );
+}
